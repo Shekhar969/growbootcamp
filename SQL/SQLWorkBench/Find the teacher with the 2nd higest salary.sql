c4d862a24e0abfc3a9ffd434e0909c260502c46c
Shekhar Rawal <shekharrawal96@gmail.com>
2026-09-24 18:21:09 +0545

    Join group oeder by queries run in mysql workbanch

--- SQL/SQLWorkBench/Find the teacher with the 2nd higest salary.sql
+++ SQL/SQLWorkBench/Find the teacher with the 2nd higest salary.sql
@@ -1,3 +1,29 @@
 SELECT salary FROM Teacher ORDER BY salary DESC LIMIT 1,1;
 
 
+SELECT AVG(salary) As AverageTeacherSalary FROM Teacher;
+
+SELECT MIN(salary) As MinimunTeacherSalary, MAX(salary) AS MaximumTeacherSalary FROM Teacher;
+
+SELECT * FROM Teacher;
+
+SELECT * FROM Depertement;
+
+SELECT s.StudentId, s.StudentFirstName, s.StudentLastName, d.DepertementName
+FROM Students s INNER JOIN Depertement d ON s.DepartmentID = d.DepertementId;
+
+SELECT d.DepertementId, d.DepertementName,s.StudentFirstName
+FROM Depertement d LEFT JOIN Students s ON d.DepertementId = s.DepartmentID;
+
+SELECT d.DepertementName,COUNT(s.StudentId) AS TotalStudents
+FROM Depertement d LEFT JOIN Students s ON d.DepertementId = s.DepartmentID
+GROUP BY d.DepertementId, d.DepertementName
+ORDER BY TotalStudents DESC;
+
+SELECT StudentFirstName, StudentLastName,
+CASE
+WHEN IsFeePaid = 1 THEN 'Paid'
+WHEN IsFeePaid = 0 THEN 'Not Paid'
+ELSE 'Unknown'
+END AS FeeStatus
+FROM Students;
